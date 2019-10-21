925f7c40442bb85b1b4ce49b4b7c822118ff9a64
rokatyy <erogatova@gmail.com>
2019-10-21 21:49:01 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,21 @@
+def if_palindrom(string):
+    """
+    This method checks if string palindrom.
+    Args:
+        string (str)
+    Returns:
+        (bool) if string is palindom returns True and False if it's not
+    """
+    return string == string[::-1]
+
+
+def count_same_values_in_list(list, value):
+    """
+    This method returns count of value in list
+    Args:
+        list (list)
+        value (int|str|bool and etc)
+    Returns:
+        (int)
+    """
+    return list.count(value)
