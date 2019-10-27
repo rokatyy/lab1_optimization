56ed4b62508229bc078c1b0bb3eeb944557a6e1b
Anna Korshikova <aniakorshikova@gmail.com>
2019-10-27 10:36:47 +0300

    Added comments to the first and third functions

--- numpy_examples.py
+++ numpy_examples.py
@@ -7,6 +7,14 @@ from scipy.optimize import minimize_scalar
 
 
 def solve_equation_system(data):
+    """
+        This method gets equation coefs and free members and returns
+        solving a system of linear equations using the Gauss method
+        Args:
+            data - coefs in csv file
+        Returns:
+            (array) - solving a system of linear equations
+    """
     equation_coefs = [item[:-1] for item in data]
     equation_free_members = [item[-1] for item in data]
 
@@ -33,12 +41,21 @@ def solve_nonlinear_equation(A, B, C):
 
 
 def minimization_by_golden_section(A, B, C):
+    """
+            This method gets coefficients of a given equation and returns
+            minimum function by the golden ratio method
+            Args:
+                A, B, C  (int) - coefs of a equation
+            Returns:
+                (float) - minimum function
+    """
+
     def f(x):
         return A * sin(B * x) * cos(C * x ** 2)
 
     return minimize_scalar(f, method='golden').x
 
 
-# print(solve_equation_system(np.genfromtxt('input_equation.csv', delimiter=',')))
-print(solve_nonlinear_equation(4,7,1))
-# print(minimization_by_golden_section(4,7,1))
+#print(solve_equation_system(np.genfromtxt('input_equation.csv', delimiter=',')))
+#print(solve_nonlinear_equation(4,7,1))
+#print(minimization_by_golden_section(4,7,100))
