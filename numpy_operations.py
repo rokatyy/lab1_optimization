e159346e9662156e309f91d842116324cc0179cc
rokatyy <erogatova@gmail.com>
2019-10-27 11:09:07 +0300

    formatting

--- numpy_operations.py
+++ numpy_operations.py
@@ -1,7 +1,5 @@
 import numpy as np
 from math import sin, cos, exp
-from scipy.optimize import fsolve
-from sympy import Symbol, nsolve
 import scipy.optimize as sopt
 from scipy.optimize import minimize_scalar
 
@@ -23,9 +21,6 @@ def solve_equation_system(data):
     return np.linalg.solve(a, b)
 
 
-
-
-
 def solve_nonlinear_equation(A, B, C):
     def fun(x):
         return A * sin(B * x[0]) * cos(C * x[1])
@@ -33,11 +28,6 @@ def solve_nonlinear_equation(A, B, C):
     xx0 = np.array([0.])  # starting point
     rslt = sopt.leastsq(fun, xx0, full_output=True)
     print("The solution is {}".format(rslt[0]))
-
-
-    # x = Symbol('x')
-    # y = Symbol('y')
-    # return nsolve([A * sin(B * x) * cos(C * y)], [x], [1, 1])
 
 
 def minimization_by_golden_section(A, B, C):
@@ -54,8 +44,3 @@ def minimization_by_golden_section(A, B, C):
         return A * sin(B * x) * cos(C * x ** 2)
 
     return minimize_scalar(f, method='golden').x
-
-
-#print(solve_equation_system(np.genfromtxt('input_equation.csv', delimiter=',')))
-#print(solve_nonlinear_equation(4,7,1))
-#print(minimization_by_golden_section(4,7,100))
