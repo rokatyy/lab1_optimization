637967f55d80bf5a9cc88215083bf948eef567ff
rokatyy <erogatova@gmail.com>
2019-11-17 11:12:00 +0300

    move to another file

---
+++ client_server/aircraft_analyze.py
@@ -0,0 +1,53 @@
+from pandas import read_csv
+
+
+def get_data_from_file(file_path):
+    """
+    This method returns data from file
+    Args:
+        file_path (str) - full file path
+    Returns:
+        data (DataFrame object)
+    """
+    data = read_csv(file_path)
+    return reformat_aircraft_info(data)
+
+
+def solver(distance, file_path):
+    """
+    This method gets distance and returns name of aircraft depends on it, IF there is no suitable aircraft method returns None
+    Args:
+        distance (int)
+        file_path (str) - fill file path
+    Returns:
+        result_name (str) - name of aircraft
+    """
+    distance = int(distance.decode('utf-8'))
+    data = reformat_aircraft_info(get_data_from_file(file_path))
+    result_name = [i for i in data if data[i] >= distance]
+    return bool(result_name) and result_name or 'No such aircraft'
+
+
+def get_max_distance(speed, time):
+    return speed * int(time)
+
+
+def reformat_aircraft_info(data):
+    """
+    This method changes the type and structure of the given data.
+    Returns a dictionary from the name and maximum range of the aircraft.
+    Args:
+        data (DataFrame object)
+    Returns:
+        distance_info (dict) where:
+                keys -- names
+                values -- max flight distance in kilometers
+    """
+    names = data['Name'].to_list()
+    distance_info = dict.fromkeys(names, 0)
+    speed = data['speed'].to_list()
+    time = data['time'].to_list()
+    for i in range(len(speed)):
+        speed[i] = int(speed[i]) / 60
+        distance_info[names[i]] = get_max_distance(speed[i], time[i])
+    return distance_info
--- client_server/server.py
+++ client_server/server.py
@@ -6,61 +6,30 @@
 список БПЛА, способных достичь цели, находящейся на заданном расстоянии, не более чем за 10 минут.
 Предусмотреть корректный ответ сервера при отсутствии требуемой информации.
 """
-
-from pandas import read_csv
+from aircraft_analyze import solver
 import socket
 
 # max time for flight in minutes
 max_flight_time = 10
 data_path = "/Users/rokatyy/PycharmProjects/methods/client_server/aircraft_info.csv"
 
+serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+serv_sock.bind(('', 53210))
+serv_sock.listen(10)
 
-def get_data_from_file(file_path):
-    """
-    This method returns data from file
-    Args:
-        file_path (str) - full file path
-    Returns:
-        data (DataFrame object)
-    """
-    data = read_csv(file_path)
-    return reformat_aircraft_info(data)
+while True:
+    # Бесконечно обрабатываем входящие подключения
+    client_sock, client_addr = serv_sock.accept()
+    print('Connected by', client_addr)
 
+    while True:
+        # Пока клиент не отключился, читаем передаваемые
+        # им данные и отправляем их обратно
+        data = client_sock.recv(1024)
+        result = solver(data, data_path)
+        if not data:
+            # Клиент отключился
+            break
+        client_sock.sendall(result)
 
-def solver(distance, data):
-    """
-    This method gets distance and returns name of aircraft depends on it, IF there is no suitable aircraft method returns None
-    Args:
-        distance (int)
-        data (DataFrame object) - data about aircrafts (name, speed, time)
-    Returns:
-        result_name (str) - name of aircraft
-    """
-    data = reformat_aircraft_info(data)
-    result_name = [i for i in data if data[i] >= distance]
-    return bool(result_name) and result_name or 'No such aircraft'
-
-
-def get_max_distance(speed, time):
-    return speed * int(time)
-
-
-def reformat_aircraft_info(data):
-    """
-    This method changes the type and structure of the given data.
-    Returns a dictionary from the name and maximum range of the aircraft.
-    Args:
-        data (DataFrame object)
-    Returns:
-        distance_info (dict) where:
-                keys -- names
-                values -- max flight distance in kilometers
-    """
-    distance_info = dict.fromkeys(data['Name'].to_list(), 0)
-    names = data['Name'].to_list()
-    speed = data['speed'].to_list()
-    time = data['time'].to_list()
-    for i in range(len(speed)):
-        speed[i] = int(speed[i]) / 60
-        distance_info[names[i]] = get_max_distance(speed[i], time[i])
-    return distance_info
+    client_sock.close()
